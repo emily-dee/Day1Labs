f7be91e05be01df0bfbb612ced8e415c4d733a23
Emily Schneider <emilydschneider@gmail.com>
2024-05-01 21:27:38 -0400

    Day 1 Lab

---
+++ main.py
@@ -0,0 +1,29 @@
+# EXERCISE 1: Echo String
+my_input = input("I'm asking a question, what is your answer? ")
+print(my_input)
+
+# EXERCISE 2: Adding a number to an integer
+my_number = int(input("Pick a number: "))
+print(my_number + 1)
+
+# EXERCISE 3: Adding a number to a float
+my_float = float(input("Pick a number: "))
+print(my_float + 0.5)
+
+# EXERCISE 4: Adding Two Floats
+my_float2 = float(input("Pick a number: "))
+my_float3 = float(input("Pick another number: "))
+float_sum = str(my_float2 + my_float3)
+print("The sum is " + float_sum)
+
+# EXERCISE 5: Multiplying Floats
+my_float4 = float(input("Pick a number: "))
+my_float5 = float(input("Pick another number: "))
+float_multiply = float(my_float4 * my_float5)
+print("The product is " + str(float_multiply))
+
+# EXERCISE 6: Dividing Integers
+my_integer1 = int(input("Enter a number: "))
+my_integer2 = int(input("Enter another number: "))
+integer_divide = int(my_integer1 / my_integer2)
+print("The result is " + str(integer_divide))
